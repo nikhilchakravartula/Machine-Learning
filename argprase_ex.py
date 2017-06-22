1ea41875f1009b527d4f5d8f930175cde9919b54
NIKHIL CHAKRAVARTULA <NIKHILCHAKRAVARTULA@GMAIL.COM>
2017-06-22 16:49:23 +0530

    adding argparse

--- argprase_ex.py
+++ argprase_ex.py
@@ -1,13 +1,21 @@
 import argparse
-
+import numpy as np
 def do_operation(args):
-	print(args)
-
+	
+	if(args.operation=='add'):
+		return np.sum(np.array(args.values))
+	elif(args.operation=='mul'):
+		return np.product(np.array(args.values))
+	else:
+		return 0
+	
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--operation",help="Addition")
+	parser.add_argument("--operation",help="Operation to be carried out. Possible operations are add,mul")
+	parser.add_argument('--values',nargs='*',help="Space seperated values on which the operation has to be done",type=float)
+	
 	
 	args = parser.parse_args()
-	do_operation(args)
+	print(do_operation(args))
 if __name__=='__main__':
 	main()
